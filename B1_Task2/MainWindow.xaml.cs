c5b618cef64c6d42fb0c2d49998b915b342fae85
Egor <e.tomashov@syberry.com>
2023-08-09 16:40:45 +0300

    Исправлен вывод

--- B1_Task2/MainWindow.xaml.cs
+++ B1_Task2/MainWindow.xaml.cs
@@ -1,22 +1,26 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Windows;
 
 using Microsoft.Win32;
 
 namespace B1_Task2
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         private SqlContext context = new SqlContext();
+        private ObservableCollection<string> loadedFiles = new ObservableCollection<string>();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            loadedFiles.CollectionChanged += LoadedFiles_CollectionChanged;
+            fileListView.ItemsSource = context.Files.Select(x => x.Id).ToList();
         }
 
-        private void LoadExcelButton_Click(object sender, RoutedEventArgs e)
+        private async void LoadExcelButton_Click(object sender, RoutedEventArgs e)
         {
             var openFileDialog = new OpenFileDialog();
             openFileDialog.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm";
@@ -24,20 +28,37 @@ namespace B1_Task2
             {
                 string filePath = openFileDialog.FileName;
                 var excelDataLoader = new ExcelDataLoader();
-                excelDataLoader.LoadDataFromExcelAsync(filePath);
-                LoadDataEntries();
-                CalculateAndDisplayTotals();
+                var fileId = await excelDataLoader.LoadDataFromExcelAsync(filePath);
+                loadedFiles.Add(fileId.ToString());
             }
         }
-        private void LoadDataEntries()
+
+        private void LoadedFiles_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
         {
-            dataGrid.ItemsSource = context.DataEntries.ToList();
+            fileListView.ItemsSource = context.Files.Select(x => x.Id).ToList(); ; // Подключаем обновленный источник данных
         }
 
-        private void CalculateAndDisplayTotals()
+        private void FileListView_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (fileListView.SelectedIndex >= 0)
+            {
+                int selectedFileId = int.Parse(fileListView.SelectedItem.ToString());
+                LoadDataEntries(selectedFileId);
+                CalculateAndDisplayTotals(selectedFileId);
+            }
+        }
+
+        private void LoadDataEntries(int fileId)
+        {
+            var data = context.DataEntries.Where(x => x.FileId == fileId).ToList();
+            dataGrid.ItemsSource = data;
+        }
+
+        private void CalculateAndDisplayTotals(int fileId)
         {
             var groupedEntries = context.DataEntries
-                .GroupBy(entry => entry.AccountCode.ToString().Substring(0,2))
+                .Where(x => x.FileId == fileId)
+                .GroupBy(entry => entry.AccountCode.ToString().Substring(0, 2))
                 .Select(group => new
                 {
                     Class = group.Key,
@@ -51,12 +72,6 @@ namespace B1_Task2
                 .OrderBy(group => group.Class);
 
             dataGridTotals.ItemsSource = groupedEntries.ToList();
-        }
-
-        private void LoadTables_Click(object sender, RoutedEventArgs e)
-        {
-            LoadDataEntries();
-            CalculateAndDisplayTotals();
         }
     }
 }
